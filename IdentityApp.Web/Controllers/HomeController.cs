08c75455783dcfe92726d14d63e9baf40be1d32a
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-11-14 00:38:04 +0300

    002-SignUp logics added.

--- IdentityApp.Web/Controllers/HomeController.cs
+++ IdentityApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using IdentityApp.Web.Models;
+using IdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace IdentityApp.Web.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly UserManager<AppUser> _userManager; // Comes from Identity API.
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
 		{
 			_logger = logger;
+			_userManager = userManager;
 		}
 
 		public IActionResult Index()
@@ -20,6 +24,38 @@ namespace IdentityApp.Web.Controllers
 
 		public IActionResult Privacy()
 		{
+			return View();
+		}
+
+		public IActionResult SignUp()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SignUp(SignUpViewModel request)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+
+			var identityResult = await _userManager.CreateAsync(new() 
+			{ 
+				UserName = request.UserName, PhoneNumber = request.PhoneNumber, Email = request.Email 
+			}, request.PasswordConfirm);
+
+			if (identityResult.Succeeded)
+			{
+				TempData["SuccessMessage"] = "Kayýt Baþarýlý.";
+				return RedirectToAction(nameof(HomeController.SignUp));
+			}
+
+			foreach (IdentityError item in identityResult.Errors)
+			{
+				ModelState.AddModelError(string.Empty, item.Description);
+			}
+
 			return View();
 		}
 
